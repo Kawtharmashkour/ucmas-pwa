ed8cf95200797626b6ad9c3f413ae34daa8188b0
yandeng63 <154352421+yandeng63@users.noreply.github.com>
2024-08-13 22:17:21 -0400

    adding avatar in navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { useAuth } from './AuthContext'; 
+import { useAuth } from './AuthContext';
 
 const Navigation = () => {
   const { authData, logout } = useAuth();
@@ -14,11 +14,11 @@ const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary fixed-top">
       <div className="container-fluid">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <NavLink className="navbar-brand" to="/">UCMAS</NavLink>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <NavLink className="nav-link" to="/">HOME</NavLink>
@@ -36,9 +36,18 @@ const Navigation = () => {
               <NavLink className="nav-link" to="/gallery">GALLERY</NavLink>
             </li>
             {authData.isLoggedIn ? (
-              <li className="nav-item">
-              <button className="nav-link" style={{ background: 'none', border: 'none', cursor: 'pointer' }} onClick={handleLogout}>SIGN OUT</button>
-            </li>
+              <>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src={authData.user.avatar || 'https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp'} className="rounded-circle" height="22" alt="Avatar" />
+                  </a>
+                  <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a className="dropdown-item" href="#">My Profile</a></li>
+                    <li><a className="dropdown-item" href="#">Settings</a></li>
+                    <li><a className="dropdown-item" href="#" onClick={handleLogout}>Logout</a></li>
+                  </ul>
+                </li>
+              </>
             ) : (
               <li className="nav-item">
                 <NavLink className="nav-link" to="/login">SIGN IN</NavLink>
